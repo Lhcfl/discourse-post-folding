07aa7594de3ae63998c6599d26cb191c7f11b57c
Lhc_fl <lhcfl@outlook.com>
2025-01-20 23:07:07 +0800

    make linter hallp

--- app/models/discourse_post_folding/post_folding_status.rb
+++ app/models/discourse_post_folding/post_folding_status.rb
@@ -6,3 +6,18 @@ module ::DiscoursePostFolding
     belongs_to :user
   end
 end
+
+# == Schema Information
+#
+# Table name: discourse_post_folding_post_folding_statuses
+#
+#  id         :bigint           not null, primary key
+#  post_id    :integer
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_discourse_post_folding_post_folding_statuses_on_post_id  (post_id)
+#
